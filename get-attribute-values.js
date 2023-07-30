2766d514f50a172c15b89d009615822b95a87a62
tikagan <30419893+tikagan@users.noreply.github.com>
2023-07-30 19:08:17 -0400

    feat: get specific attribute's values

--- get-attribute-values.js
+++ get-attribute-values.js
@@ -3,15 +3,17 @@ import 'dotenv/config'
 
 const apiToken = process.env.EXIST_API_TOKEN
 
-const url = 'https://exist.io/api/2/attributes/with-values/'
+const attribute = ['coffees']
+
+const url = `https://exist.io/api/2/attributes/values/?attribute=${attribute}`
 
 const config = {
-  headers: { 'Authorization': `Bearer ${apiToken}` }
+  headers: { 'Authorization': `Bearer ${apiToken}` },
 }
 
 axios.get(url, config)
   .then((res) => {
-    console.log('Results: ', res.data)
+    console.log('Results: ', res.data.results)
   })
   .catch((e) => {
     console.log('e ', e)
