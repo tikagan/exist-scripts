1c2cf1f1f3693e1ddd8d931670937f87527619c2
tikagan <30419893+tikagan@users.noreply.github.com>
2023-07-30 19:09:06 -0400

    feat: post-increment-attribute-values

---
+++ post-increment-attribute-value.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import 'dotenv/config'
+
+const apiToken = process.env.EXIST_API_TOKEN
+
+const url = 'https://exist.io/api/2/attributes/increment/'
+
+const config = {
+  headers: {
+    'Authorization': `Bearer ${apiToken}`,
+    'Content-Type': 'application/json'
+  }
+}
+
+const data = JSON.stringify([
+  {
+    name: 'coffees',
+    value: 1
+  }
+])
+
+axios.post(url, data, config)
+.then((res) => {
+  console.log(res.data)
+
+  })
+  .catch((e) => {
+    console.log('e ', e)
+  })
